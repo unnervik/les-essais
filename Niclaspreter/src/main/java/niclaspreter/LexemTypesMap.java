ead6243c39fb131b5ee80e48d2df35ae6d12e6ec
Niclas Unnervik <nic.unn@gmail.com>
2020-04-12 16:28:58 +0200

    Added preter

---
+++ Niclaspreter/src/main/java/niclaspreter/LexemTypesMap.java
@@ -0,0 +1,7 @@
+package niclaspreter;
+
+import java.util.HashMap;
+
+public class LexemTypesMap {
+    static HashMap<LexemType, String> name = new HashMap<>();
+}
---
+++ Niclaspreter/src/main/java/niclaspreter/Main.java
@@ -0,0 +1,18 @@
+package niclaspreter;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) {
+        String filename;
+        if (args.length == 2) {
+            filename = args[1];
+        } else {
+            filename = "/Users/niun/Niclaspreter/data/data1.nic";
+        }
+        System.out.println("using file: " + filename);
+
+        Lexer lexer = new Lexer(filename);
+        lexer.lexFile();
+    }
+}
